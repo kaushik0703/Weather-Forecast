beb6bab24d8cffecc5225dfa0e284e547e4ff91a
kaushik0703 <106008167+kaushik0703@users.noreply.github.com>
2023-04-07 14:46:25 +0530

    Update 1_express.js

--- src/1_express.js
+++ src/1_express.js
@@ -19,7 +19,7 @@ const geocoder = NodeGeocoder(options);
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
